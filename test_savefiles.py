06571b9dab16e894b56b0c18ebc51f7c0e6a60fe
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-06-29 11:35:15 -0400

    Create test_savefiles.py

---
+++ test_savefiles.py
@@ -0,0 +1,21 @@
+import utime
+from machine import Pin
+
+solenoid = Pin(16, Pin.OUT)
+led = Pin(25, Pin.OUT)
+lick = Pin(14, Pin.IN, Pin.PULL_DOWN)
+solenoidOpenTimestamps = []
+# lickTimestamps = []
+file = open("solenoidOpenTimestamps.txt","w")
+
+while True:
+    if lick.value():
+        led.toggle()
+        solenoid.value(0)
+        utime.sleep(2)
+        solenoid.value(1)
+        data = solenoidOpenTimestamps.append(utime.time())
+        file.write(str(data)+",")
+        file.flush()
+        utime.sleep(2)
+file.close()
