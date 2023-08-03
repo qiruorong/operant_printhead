3c69e6326041e3b9355367b889c0df27bfd74863
Ruorong Qi <129242594+qiruorong@users.noreply.github.com>
2023-08-03 16:01:28 -0400

    Update brush.ino

--- brush_arduino.ino
+++ brush_arduino.ino
@@ -1,3 +1,4 @@
+#include <AFMotor.h>
 
 // define enA = 9
 // define in1 = 6
@@ -11,35 +12,6 @@ void setup() {
 
   Serial.begin(9600);
 }
-
-void loop() {
-  int potValue = analogRead(A0); // potentiometer
-  int motorSpeed = map(potValue, 0, 1023, 0 , 255); 
-  analogWrite(enA, motorSpeed); // Send to L298N
-  int randomValue = random(2);
-
-    if (randomValue = 0) {
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        wheelDirection = 1;
-
-    else {
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        
-    delay(200);
-        }
-    }
-}
-
-
-
-
-
-
-
-
-#include <AFMotor.h>
 
 AF_DCMotor motor(1); // channel 1
 
